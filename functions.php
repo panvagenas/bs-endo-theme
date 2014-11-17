06e3ce4036d2026690e94b89a4baa4250e336058
Panagiotis Vagenas <pan.vagenas@gmail.com>
2014-11-17 23:05:40 +0200

    empty line after php closing tag causes xml errors

--- functions.php
+++ functions.php
@@ -9,10 +9,10 @@ sidebars, comments, ect.
 */
 
 // LOAD BONES CORE (if you remove this, the theme will break)
-require_once( 'library/bones.php' );
+require_once('library/bones.php');
 
 // USE THIS TEMPLATE TO CREATE CUSTOM POST TYPES EASILY
-require_once( 'library/custom-post-type.php' );
+require_once('library/custom-post-type.php');
 
 // CUSTOMIZE THE WORDPRESS ADMIN (off by default)
 // require_once( 'library/admin.php' );
@@ -22,61 +22,62 @@ require_once( 'library/custom-post-type.php' );
  * Let's get everything up and running.
  *********************/
 
-function bones_ahoy() {
+function bones_ahoy()
+{
 
 	//Allow editor style.
 //  add_editor_style();
 
 	// let's get language support going, if you need it
-	load_theme_textdomain( 'bonestheme', get_template_directory() . '/library/translation' );
+	load_theme_textdomain('bonestheme', get_template_directory().'/library/translation');
 
 	// launching operation cleanup
-	add_action( 'init', 'bones_head_cleanup' );
+	add_action('init', 'bones_head_cleanup');
 	// A better title
-	add_filter( 'wp_title', 'rw_title', 10, 3 );
+	add_filter('wp_title', 'rw_title', 10, 3);
 	// remove WP version from RSS
-	add_filter( 'the_generator', 'bones_rss_version' );
+	add_filter('the_generator', 'bones_rss_version');
 	// remove pesky injected css for recent comments widget
-	add_filter( 'wp_head', 'bones_remove_wp_widget_recent_comments_style', 1 );
+	add_filter('wp_head', 'bones_remove_wp_widget_recent_comments_style', 1);
 	// clean up comment styles in the head
-	add_action( 'wp_head', 'bones_remove_recent_comments_style', 1 );
+	add_action('wp_head', 'bones_remove_recent_comments_style', 1);
 	// clean up gallery output in wp
-	add_filter( 'gallery_style', 'bones_gallery_style' );
+	add_filter('gallery_style', 'bones_gallery_style');
 
 	// enqueue base scripts and styles
-	add_action( 'wp_enqueue_scripts', 'bones_scripts_and_styles', 999 );
+	add_action('wp_enqueue_scripts', 'bones_scripts_and_styles', 999);
 	// ie conditional wrapper
 
 	// launching this stuff after theme setup
 	bones_theme_support();
 
 	// adding sidebars to Wordpress (these are created in functions.php)
-	add_action( 'widgets_init', 'bones_register_sidebars' );
+	add_action('widgets_init', 'bones_register_sidebars');
 
 	// cleaning up random code around images
-	add_filter( 'the_content', 'bones_filter_ptags_on_images' );
+	add_filter('the_content', 'bones_filter_ptags_on_images');
 	// cleaning up excerpt
-	add_filter( 'excerpt_more', 'bones_excerpt_more' );
-	add_filter( 'get_the_excerpt', 'bones_get_the_excerpt' );
-	add_shortcode( 'gallery', 'bones_gallery_shortcode' );
+	add_filter('excerpt_more', 'bones_excerpt_more');
+	add_filter('get_the_excerpt', 'bones_get_the_excerpt');
+	add_shortcode('gallery', 'bones_gallery_shortcode');
 } /* end bones ahoy */
 
 // let's get this party started
-add_action( 'after_setup_theme', 'bones_ahoy' );
+add_action('after_setup_theme', 'bones_ahoy');
 
 
 /************* OEMBED SIZE OPTIONS *************/
 
-if ( ! isset( $content_width ) ) {
+if (!isset($content_width)) {
 	$content_width = 820;
 }
 
 /************* THUMBNAIL SIZE OPTIONS *************/
 
 // Thumbnail sizes
-add_image_size( 'large', 820, 510, true );
-add_image_size( 'bones-thumb-600', 600, 150, true );
-add_image_size( 'bones-thumb-300', 300, 100, true );
+add_image_size('large', 820, 510, true);
+add_image_size('bones-thumb-600', 600, 150, true);
+add_image_size('bones-thumb-300', 300, 100, true);
 
 /*
 to add more sizes, simply copy a line from above
@@ -98,14 +99,15 @@ You can change the names and dimensions to whatever
 you like. Enjoy!
 */
 
-add_filter( 'image_size_names_choose', 'bones_custom_image_sizes' );
+add_filter('image_size_names_choose', 'bones_custom_image_sizes');
 
-function bones_custom_image_sizes( $sizes ) {
-	return array_merge( $sizes, array(
-		'large'           => __( '820px by 510px' ),
-		'bones-thumb-600' => __( '600px by 150px' ),
-		'bones-thumb-300' => __( '300px by 100px' ),
-	) );
+function bones_custom_image_sizes($sizes)
+{
+	return array_merge($sizes, array(
+		'large'           => __('820px by 510px'),
+		'bones-thumb-600' => __('600px by 150px'),
+		'bones-thumb-300' => __('300px by 100px'),
+	));
 }
 
 /*
@@ -119,175 +121,176 @@ new image size.
 /************* ACTIVE SIDEBARS ********************/
 
 // Sidebars & Widgetizes Areas
-function bones_register_sidebars() {
-	register_sidebar( array(
+function bones_register_sidebars()
+{
+	register_sidebar(array(
 		'id'            => 'sidebar1',
-		'name'          => __( 'Main Sidebar', 'bonestheme' ),
-		'description'   => __( 'The first (primary) sidebar.', 'bonestheme' ),
+		'name'          => __('Main Sidebar', 'bonestheme'),
+		'description'   => __('The first (primary) sidebar.', 'bonestheme'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h4 class="widgettitle">',
 		'after_title'   => '</h4>',
-	) );
+	));
 
-	register_sidebar( array(
+	register_sidebar(array(
 		'id'            => 'home_sidebar',
-		'name'          => __( 'Home Mainbar', 'bonestheme' ),
-		'description'   => __( 'The home sidebar that displayed in main content area.', 'bonestheme' ),
+		'name'          => __('Home Mainbar', 'bonestheme'),
+		'description'   => __('The home sidebar that displayed in main content area.', 'bonestheme'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h4 class="widgettitle">',
 		'after_title'   => '</h4>',
-	) );
+	));
 
-	register_sidebar( array(
+	register_sidebar(array(
 		'id'            => 'footer_sidebar_left',
-		'name'          => __( 'Footer Left Sidebar |*|_|_|', 'bonestheme' ),
-		'description'   => __( 'The footer left sidebar.', 'bonestheme' ),
+		'name'          => __('Footer Left Sidebar |*|_|_|', 'bonestheme'),
+		'description'   => __('The footer left sidebar.', 'bonestheme'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h4 class="widgettitle">',
 		'after_title'   => '</h4>',
-	) );
+	));
 
-	register_sidebar( array(
+	register_sidebar(array(
 		'id'            => 'footer_sidebar_center',
-		'name'          => __( 'Footer Center Sidebar |_|*|_|', 'bonestheme' ),
-		'description'   => __( 'The footer center sidebar.', 'bonestheme' ),
+		'name'          => __('Footer Center Sidebar |_|*|_|', 'bonestheme'),
+		'description'   => __('The footer center sidebar.', 'bonestheme'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h4 class="widgettitle">',
 		'after_title'   => '</h4>',
-	) );
+	));
 
-	register_sidebar( array(
+	register_sidebar(array(
 		'id'            => 'footer_sidebar_right',
-		'name'          => __( 'Footer Right Sidebar |_|_|*|', 'bonestheme' ),
-		'description'   => __( 'The footer right sidebar.', 'bonestheme' ),
+		'name'          => __('Footer Right Sidebar |_|_|*|', 'bonestheme'),
+		'description'   => __('The footer right sidebar.', 'bonestheme'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h4 class="widgettitle">',
 		'after_title'   => '</h4>',
-	) );
-	register_sidebar( array(
+	));
+	register_sidebar(array(
 		'id'            => 'footer_sidebar_right_1',
-		'name'          => __( 'Footer Right Sidebar |_|_|_|*|', 'bonestheme' ),
-		'description'   => __( 'The footer right sidebar 2.', 'bonestheme' ),
+		'name'          => __('Footer Right Sidebar |_|_|_|*|', 'bonestheme'),
+		'description'   => __('The footer right sidebar 2.', 'bonestheme'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h4 class="widgettitle">',
 		'after_title'   => '</h4>',
-	) );
+	));
 
-	register_sidebar( array(
+	register_sidebar(array(
 		'id'            => 'header_sidebar_center',
-		'name'          => __( 'Header Center Sidebar', 'bonestheme' ),
-		'description'   => __( 'The header center sidebar.', 'bonestheme' ),
+		'name'          => __('Header Center Sidebar', 'bonestheme'),
+		'description'   => __('The header center sidebar.', 'bonestheme'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h4 class="widgettitle">',
 		'after_title'   => '</h4>',
-	) );
+	));
 
-	register_sidebar( array(
+	register_sidebar(array(
 		'id'            => 'header_sidebar_right',
-		'name'          => __( 'Header Right Sidebar', 'bonestheme' ),
-		'description'   => __( 'The header right sidebar.', 'bonestheme' ),
+		'name'          => __('Header Right Sidebar', 'bonestheme'),
+		'description'   => __('The header right sidebar.', 'bonestheme'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h4 class="widgettitle">',
 		'after_title'   => '</h4>',
-	) );
+	));
 
-	register_sidebar( array(
+	register_sidebar(array(
 		'id'            => 'home_banners',
-		'name'          => __( 'Home Banners Area', 'bonestheme' ),
-		'description'   => __( 'Main banner area.', 'bonestheme' ),
+		'name'          => __('Home Banners Area', 'bonestheme'),
+		'description'   => __('Main banner area.', 'bonestheme'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h4 class="widgettitle">',
 		'after_title'   => '</h4>',
-	) );
+	));
 
-	register_sidebar( array(
+	register_sidebar(array(
 		'id'            => 'under_home_banners_left',
-		'name'          => __( 'Bellow Home Banners Area Left', 'bonestheme' ),
-		'description'   => __( 'Bellow Home Banners Area Left', 'bonestheme' ),
+		'name'          => __('Bellow Home Banners Area Left', 'bonestheme'),
+		'description'   => __('Bellow Home Banners Area Left', 'bonestheme'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h4 class="widgettitle">',
 		'after_title'   => '</h4>',
-	) );
+	));
 
-	register_sidebar( array(
+	register_sidebar(array(
 		'id'            => 'under_home_banners_right',
-		'name'          => __( 'Bellow Home Banners Area Right', 'bonestheme' ),
-		'description'   => __( 'Bellow Home Banners Area Right', 'bonestheme' ),
+		'name'          => __('Bellow Home Banners Area Right', 'bonestheme'),
+		'description'   => __('Bellow Home Banners Area Right', 'bonestheme'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h4 class="widgettitle">',
 		'after_title'   => '</h4>',
-	) );
+	));
 
-	register_sidebar( array(
+	register_sidebar(array(
 		'id'            => 'under_home_banners_left_under',
-		'name'          => __( 'Bellow Home Banners Area Left Under', 'bonestheme' ),
-		'description'   => __( 'Bellow Home Banners Area Left Under', 'bonestheme' ),
+		'name'          => __('Bellow Home Banners Area Left Under', 'bonestheme'),
+		'description'   => __('Bellow Home Banners Area Left Under', 'bonestheme'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h4 class="widgettitle">',
 		'after_title'   => '</h4>',
-	) );
+	));
 
-	register_sidebar( array(
+	register_sidebar(array(
 		'id'            => 'under_home_banners_right_under',
-		'name'          => __( 'Bellow Home Banners Area Right Under', 'bonestheme' ),
-		'description'   => __( 'Bellow Home Banners Area Right Under', 'bonestheme' ),
+		'name'          => __('Bellow Home Banners Area Right Under', 'bonestheme'),
+		'description'   => __('Bellow Home Banners Area Right Under', 'bonestheme'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h4 class="widgettitle">',
 		'after_title'   => '</h4>',
-	) );
+	));
 
-	register_sidebar( array(
+	register_sidebar(array(
 		'id'            => 'adv1_sidebar',
-		'name'          => __( 'Adv 1 Sidebar', 'bonestheme' ),
-		'description'   => __( 'Sidebar advertisement position 1.', 'bonestheme' ),
+		'name'          => __('Adv 1 Sidebar', 'bonestheme'),
+		'description'   => __('Sidebar advertisement position 1.', 'bonestheme'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h4 class="widgettitle">',
 		'after_title'   => '</h4>',
-	) );
+	));
 
-	register_sidebar( array(
+	register_sidebar(array(
 		'id'            => 'adv2_sidebar',
-		'name'          => __( 'Adv 2 Sidebar', 'bonestheme' ),
-		'description'   => __( 'Sidebar advertisement position 2.', 'bonestheme' ),
+		'name'          => __('Adv 2 Sidebar', 'bonestheme'),
+		'description'   => __('Sidebar advertisement position 2.', 'bonestheme'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h4 class="widgettitle">',
 		'after_title'   => '</h4>',
-	) );
+	));
 
-	register_sidebar( array(
+	register_sidebar(array(
 		'id'            => 'post_top_sidebar',
-		'name'          => __( 'Post Top Area', 'bonestheme' ),
-		'description'   => __( 'Above posts in single page', 'bonestheme' ),
+		'name'          => __('Post Top Area', 'bonestheme'),
+		'description'   => __('Above posts in single page', 'bonestheme'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h4 class="widgettitle">',
 		'after_title'   => '</h4>',
-	) );
+	));
 
-	register_sidebar( array(
+	register_sidebar(array(
 		'id'            => 'post_bottom_sidebar',
-		'name'          => __( 'Below Posts Area', 'bonestheme' ),
-		'description'   => __( 'Bellow posts in single page', 'bonestheme' ),
+		'name'          => __('Below Posts Area', 'bonestheme'),
+		'description'   => __('Bellow posts in single page', 'bonestheme'),
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h4 class="widgettitle">',
 		'after_title'   => '</h4>',
-	) );
+	));
 
 	/*
 	to add more sidebars or widgetized areas, just copy
@@ -321,7 +324,7 @@ function bones_register_sidebars() {
 // Comment Layout
 function bones_comments($comment, $args, $depth) {
 $GLOBALS['comment'] = $comment; ?>
-<div id="comment-<?php comment_ID(); ?>" <?php comment_class( 'cf' ); ?>>
+<div id="comment-<?php comment_ID(); ?>" <?php comment_class('cf'); ?>>
 	<article class="cf">
 		<header class="comment-author vcard">
 			<?php
@@ -335,28 +338,28 @@ $GLOBALS['comment'] = $comment; ?>
 			// create variable
 			$bgauthemail = get_comment_author_email();
 			?>
-			<img data-gravatar="http://www.gravatar.com/avatar/<?php echo md5( $bgauthemail ); ?>?s=40"
+			<img data-gravatar="http://www.gravatar.com/avatar/<?php echo md5($bgauthemail); ?>?s=40"
 			     class="load-gravatar avatar avatar-48 photo" height="40" width="40"
 			     src="<?php echo get_template_directory_uri(); ?>/library/images/nothing.gif"/>
 			<?php // end custom gravatar call ?>
-			<?php printf( __( '<cite class="fn">%1$s</cite> %2$s', 'bonestheme' ), get_comment_author_link(), edit_comment_link( __( '(Edit)', 'bonestheme' ), '  ', '' ) ) ?>
-			<time datetime="<?php echo comment_time( 'Y-m-j' ); ?>"><a
-					href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php comment_time( __( 'F jS, Y', 'bonestheme' ) ); ?> </a>
+			<?php printf(__('<cite class="fn">%1$s</cite> %2$s', 'bonestheme'), get_comment_author_link(), edit_comment_link(__('(Edit)', 'bonestheme'), '  ', '')) ?>
+			<time datetime="<?php echo comment_time('Y-m-j'); ?>"><a
+					href="<?php echo htmlspecialchars(get_comment_link($comment->comment_ID)) ?>"><?php comment_time(__('F jS, Y', 'bonestheme')); ?> </a>
 			</time>
 
 		</header>
-		<?php if ( $comment->comment_approved == '0' ) : ?>
+		<?php if ($comment->comment_approved == '0') : ?>
 			<div class="alert alert-info">
-				<p><?php _e( 'Your comment is awaiting moderation.', 'bonestheme' ) ?></p>
+				<p><?php _e('Your comment is awaiting moderation.', 'bonestheme') ?></p>
 			</div>
 		<?php endif; ?>
 		<section class="comment_content cf">
 			<?php comment_text() ?>
 		</section>
-		<?php comment_reply_link( array_merge( $args, array(
+		<?php comment_reply_link(array_merge($args, array(
 			'depth'     => $depth,
 			'max_depth' => $args['max_depth']
-		) ) ) ?>
+		))) ?>
 	</article>
 	<?php // </li> is added by WordPress automatically ?>
 	<?php
@@ -370,14 +373,15 @@ $GLOBALS['comment'] = $comment; ?>
 	can replace these fonts, change it in your scss files
 	and be up and running in seconds.
 	*/
-	function bones_fonts() {
+	function bones_fonts()
+	{
 //		wp_register_style( 'googleFontsBones', 'http://fonts.googleapis.com/css?family=Open+Sans|GFS+Didot|Ubuntu+Mono&subset=latin,greek' );
 //		wp_enqueue_style( 'googleFontsBones' );
 	}
 
-	add_action( 'wp_print_styles', 'bones_fonts' );
+	add_action('wp_print_styles', 'bones_fonts');
 
 
 
 	/* DON'T DELETE THIS CLOSING TAG */
-	?>
+?>
